eed836b34a9da8ae2e5e49946c2a3c73f173204d
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 17:03:24 +0300

    инициализация файлов

---
+++ drafts.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+test_file = 'test_1.csv'
+prod_file = 'recommends.csv'
+
+TEST_RUN = {
+    'sku': "fVFCOZ5AFF",
+    'coef': 0,
+    'csv_file': test_file,
+}
+
+PROD_RUN = {
+    'sku': "fVFCOZ5AFF",
+    'coef': 0,
+    'csv_file': prod_file,
+}
