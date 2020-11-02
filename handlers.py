258676478881c9f8e826501150e48858859c02f3
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 16:40:47 +0300

    переписал код в объектном стиле

--- handlers.py
+++ handlers.py
@@ -6,61 +6,82 @@ from decorators import timer
 OUTPUT_FILE = 'recommends.csv'
 
 
-class FileReader(multiprocessing.Process):
-    def __init__(self, dataset, sku, coef, matches, number):
+class FileReader:
+    """Базовый класс для обработчика файла"""
+
+    def __init__(self, sku, coef, csv_file=OUTPUT_FILE):
+        self.sku = sku
+        self.coef = coef
+        self.csv_file = csv_file
+        self.matches = []
+
+    def recommend_collector(self, line):
+        return True if line[1] == self.sku and float(line[2]) >= self.coef else False
+
+
+class SingeProcessFileReader(FileReader):
+    @timer
+    def run(self):
+        print("однопроцессорный способ:")
+
+        with open(self.csv_file, 'r', newline='') as csv_file:
+            csv_data = csv.reader(csv_file)
+            count = 0
+
+            for row in csv_data:
+                self.recommend_collector(row) and self.matches.append(row[0])
+                count += 1
+            print(self.matches)
+            return self.matches
+
+
+class FileReaderProcess(multiprocessing.Process):
+    def __init__(self, dataset, sku, coef, matches):
         super().__init__()
         self.dataset = dataset
         self.sku = sku
         self.coef = coef
         self.matches = matches
         self.count = 0
-        self.number = number
 
     def run(self):
         for row in self.dataset:
-            self.is_it(row)
+            self.process(row)
             self.count += 1
 
-    def is_it(self, row):
+    def process(self, row):
         if len(row) < 3:
-            return False
+            # если попадается невалидная строка, то мы ее пропускаем (при желании, можно отдавать её в логгер)
+            # print(f'problem with {row}', flush=True)
+            return
         if row[1] == self.sku and float(row[2]) >= self.coef:
             self.matches.put(row[0])
 
 
-@timer
-def recommends_reader(sku, coef, csv_file=OUTPUT_FILE):
-    """
-    :param sku: идентификатор товарной позиции
-    :param coef: показатель совпадения
-    :param csv_file: файл с коэффициентами рекомендаций
-    :return: список рекомендация для sku, с коэффициентом совпадений >= coef
-    """
+class MultiprocessingFileReader(FileReader):
+    def __init__(self, sku, coef, csv_file=OUTPUT_FILE):
+        super().__init__(sku, coef, csv_file)
+        self.matches = multiprocessing.Queue()
+        self.match_list = []
+        self.proc_num = 4
 
-    with open(csv_file, 'r', newline='') as csv_file:
-        csv_data = csv.reader(csv_file)
-        matches = []
-        for row in csv_data:
-            if row[1] == sku and float(row[2]) >= coef:
-                matches.append(row[0])
-        return matches
+    @timer
+    def run(self):
+        print("многопроцессорный способ:")
+        with open(self.csv_file, 'r', newline='') as csv_file:
+            csv_data = csv.reader(csv_file)
+
+            for i in range(self.proc_num):
+                proc = FileReaderProcess(dataset=csv_data, sku=self.sku, coef=self.coef, matches=self.matches)
+                proc.start()
+
+            for i in range(self.proc_num):
+                proc.join()
+
+        while not self.matches.empty():
+            self.match_list.append(self.matches.get())
+        return self.match_list
 
 
-def recommends_reader_mp(sku, coef, csv_file='recommends.csv'):
-    print("многопроцессорный способ:")
-    matches = multiprocessing.Queue()
-    match_list = []
-    with open(csv_file, 'r', newline='') as csv_file:
-        csv_data = csv.reader(csv_file)
-        proc_num = 4
-
-        for i in range(proc_num):
-            proc = FileReader(dataset=csv_data, sku=sku, coef=coef, number=i, matches=matches)
-            proc.start()
-        for i in range(proc_num):
-            proc.join()
-
-    while True:
-        if matches.empty():
-            return match_list
-        match_list.append(matches.get())
+if __name__ == '__main__':
+    pass
