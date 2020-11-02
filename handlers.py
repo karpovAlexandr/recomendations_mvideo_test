10432563190c599c7e7f01a72e0eaf1a9600175c
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 13:18:34 +0300

    реализован способ обработки через мультипроцессы

--- handlers.py
+++ handlers.py
@@ -1,24 +1,42 @@
 import csv
-import time
+import multiprocessing
+
+from decorators import timer
+
+OUTPUT_FILE = 'recommends.csv'
 
 
-def timer(func):
-    def surrogate(*args, **kwargs):
-        started_at = time.time()
+class FileReader(multiprocessing.Process):
+    def __init__(self, dataset, sku, coef, matches, number):
+        super().__init__()
+        self.dataset = dataset
+        self.sku = sku
+        self.coef = coef
+        self.matches = matches
+        self.count = 0
+        self.number = number
 
-        result = func(*args, **kwargs)
+    def run(self):
+        for row in self.dataset:
+            self.is_it(row)
+            self.count += 1
 
-        ended_at = time.time()
-        elapsed = round(ended_at - started_at, 4)
-        print(f'Время работы - {elapsed} сек.')
-
-        return result
-
-    return surrogate
+    def is_it(self, row):
+        if len(row) < 3:
+            return False
+        if row[1] == self.sku and float(row[2]) >= self.coef:
+            self.matches.put(row[0])
 
 
 @timer
-def recommends_reader(sku, coef, csv_file='recommends.csv'):
+def recommends_reader(sku, coef, csv_file=OUTPUT_FILE):
+    """
+    :param sku: идентификатор товарной позиции
+    :param coef: показатель совпадения
+    :param csv_file: файл с коэффициентами рекомендаций
+    :return: список рекомендация для sku, с коэффициентом совпадений >= coef
+    """
+
     with open(csv_file, 'r', newline='') as csv_file:
         csv_data = csv.reader(csv_file)
         matches = []
@@ -26,3 +44,23 @@ def recommends_reader(sku, coef, csv_file='recommends.csv'):
             if row[1] == sku and float(row[2]) >= coef:
                 matches.append(row[0])
         return matches
+
+
+def recommends_reader_mp(sku, coef, csv_file='recommends.csv'):
+    print("многопроцессорный способ:")
+    matches = multiprocessing.Queue()
+    match_list = []
+    with open(csv_file, 'r', newline='') as csv_file:
+        csv_data = csv.reader(csv_file)
+        proc_num = 4
+
+        for i in range(proc_num):
+            proc = FileReader(dataset=csv_data, sku=sku, coef=coef, number=i, matches=matches)
+            proc.start()
+        for i in range(proc_num):
+            proc.join()
+
+    while True:
+        if matches.empty():
+            return match_list
+        match_list.append(matches.get())
