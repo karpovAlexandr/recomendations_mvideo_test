f803ad393929d63629616812db5bea7b437ecfc1
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 13:17:50 +0300

    декоратор вынесен в отдельный файл

---
+++ decorators.py
@@ -0,0 +1,16 @@
+import time
+
+
+def timer(func):
+    def surrogate(*args, **kwargs):
+        started_at = time.time()
+
+        result = func(*args, **kwargs)
+
+        ended_at = time.time()
+        elapsed = round(ended_at - started_at, 4)
+        print(f'Время работы - {elapsed} сек.')
+
+        return result
+
+    return surrogate
