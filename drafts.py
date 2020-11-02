48b6673454b6ed673983e8d9de60c5af086b23b2
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 18:59:41 +0300

    добавил черонвики методов

--- drafts.py
+++ drafts.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import multiprocessing
+
+import pandas as pd
+
+from decorators import timer
+import handlers
 
 test_file = 'test_1.csv'
 prod_file = 'recommends.csv'
@@ -14,3 +20,67 @@ PROD_RUN = {
     'coef': 0,
     'csv_file': prod_file,
 }
+
+
+def process():
+    """функция пустышка"""
+    pass
+
+
+@timer
+def file_reader_chunk_jobs(sku, coef, csv_file):
+    print('вариант с джобами и чанками')
+    # https://www.blopig.com/blog/2016/08/processing-large-files-using-python/
+    jobs = []
+    chunksize = 10 ** 5
+    count = 0
+    pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    for chunk in pd.read_csv(csv_file, chunksize=chunksize, names=('sku', 'rec', 'coef')):
+
+        for line in chunk.values:
+            jobs.append(pool.apply_async(process, args=(line,)))
+
+        for job in jobs:
+            job.get()
+        count += 1
+    pool.close()
+
+
+class MultiprocessingFRVer2(handlers.FileReader):
+    def __init__(self, sku, coef, csv_file):
+        super().__init__(sku, coef, csv_file)
+        self.matches = multiprocessing.Queue()
+        self.match_list = set()
+        self.proc_num = multiprocessing.cpu_count()
+        self.chunksize = 10 ** 7
+
+    @timer
+    def run(self):
+        print("многопроцессорный способ вариант с чанками:")
+
+        for chunk in pd.read_csv(self.csv_file, chunksize=self.chunksize, names=('sku', 'rec', 'coef',)):
+            processes = []
+            for i in range(self.proc_num):
+                processes.append(
+                    handlers.FileReaderProcess(
+                        dataset=chunk.values,
+                        sku=self.sku,
+                        coef=self.coef,
+                        matches=self.matches)
+                )
+            for proc in processes:
+                proc.start()
+
+            for proc in processes:
+                proc.join()
+
+        while True:
+            if self.matches.empty():
+                print(self.match_list)
+                return False
+            self.match_list.add(self.matches.get())
+
+
+x = MultiprocessingFRVer2(**PROD_RUN)
+x.run()
+
