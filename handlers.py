45211e68c2f5b819a244de3241de9eaa94372d50
alexandr <karpov.a1exandr@yandex.ru>
2020-11-02 17:04:28 +0300

    добавил докстринги, убрал лишние принты

--- handlers.py
+++ handlers.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import csv
 import multiprocessing
 
 from decorators import timer
+
 
 OUTPUT_FILE = 'recommends.csv'
 
@@ -20,22 +23,27 @@ class FileReader:
 
 
 class SingeProcessFileReader(FileReader):
+    """Обработчик файла строка за строкой, в одном процессе"""
+
     @timer
     def run(self):
-        print("однопроцессорный способ:")
-
         with open(self.csv_file, 'r', newline='') as csv_file:
             csv_data = csv.reader(csv_file)
             count = 0
 
             for row in csv_data:
+                if len(row) < 3:
+                    return
+
                 self.recommend_collector(row) and self.matches.append(row[0])
                 count += 1
-            print(self.matches)
+
             return self.matches
 
 
 class FileReaderProcess(multiprocessing.Process):
+    """Обработчик для датасета"""
+
     def __init__(self, dataset, sku, coef, matches):
         super().__init__()
         self.dataset = dataset
@@ -51,7 +59,8 @@ class FileReaderProcess(multiprocessing.Process):
 
     def process(self, row):
         if len(row) < 3:
-            # если попадается невалидная строка, то мы ее пропускаем (при желании, можно отдавать её в логгер)
+            # если попадается невалидная строка, то мы ее пропускаем (при желании, можно отдавать её в логгер или на
+            # консоль)
             # print(f'problem with {row}', flush=True)
             return
         if row[1] == self.sku and float(row[2]) >= self.coef:
@@ -59,15 +68,19 @@ class FileReaderProcess(multiprocessing.Process):
 
 
 class MultiprocessingFileReader(FileReader):
+    """
+    Обработчки файла строка за строкой в многопроцессорном режиме
+    (число процессов по умолчанию равно числу ядер на машине)
+    """
+
     def __init__(self, sku, coef, csv_file=OUTPUT_FILE):
         super().__init__(sku, coef, csv_file)
         self.matches = multiprocessing.Queue()
         self.match_list = []
-        self.proc_num = 4
+        self.proc_num = multiprocessing.cpu_count()
 
     @timer
     def run(self):
-        print("многопроцессорный способ:")
         with open(self.csv_file, 'r', newline='') as csv_file:
             csv_data = csv.reader(csv_file)
 
